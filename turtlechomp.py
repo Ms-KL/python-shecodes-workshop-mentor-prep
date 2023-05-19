518e68fb775cec03513031a8bb884964e0d25125
Ms-KL <thekristyleigh@gmail.com>
2023-05-19 10:02:29 +0800

    Add step 2.3 - define functions for moving turtle
    Create user defined functions to move turtle.
    Set degrees of movement for left and right keys.
    Increase speed with up key

--- turtlechomp.py
+++ turtlechomp.py
@@ -24,6 +24,17 @@ player.penup() # won't leave a line as the turtle moves
 # ! STEP 1.7: set speed variable
 speed = 1
 
+# ! STEP 2.3: define functions for moving turtle
+def turn_left():
+    player.left(30) # move turtle by 30 degrees when left arrow is pressed
+
+def turn_right():
+    player.right(30) # move turtle by 30 degrees when right arrow is pressed
+
+def increase_speed():
+    global speed # moving out of local function into global environment
+    speed += 1 # increase speed by 1 with every keystroke up
+
 # ! STEP 2.2: set keyboard binding
 turtle.listen() # listen for keystroke (onkey method)
 turtle.onkey(turn_left, 'Left')
