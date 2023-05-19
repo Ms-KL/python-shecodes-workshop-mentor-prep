2d1e1b3738a07826f0cf9165e18f4068675e092b
Ms-KL <thekristyleigh@gmail.com>
2023-05-19 09:58:01 +0800

    Add step 2.2 - set keyboard binding
    Add listen + onkey methods to run function when arrow is pressed

--- turtlechomp.py
+++ turtlechomp.py
@@ -5,26 +5,38 @@ import turtle
 # ! MODULE 1: 
 #  https://tutorials.shecodes.com.au/python/space_turtle_chomp/part_1_space_turtle_chomp/
 
-# ! STEP 5: setup screen
+# ! MODULE 2:
+# https://tutorials.shecodes.com.au/python/space_turtle_chomp/part_2__turn_baby_turn/
+
+# __________________________
+
+# ! STEP 1.5: setup screen
 turtle.setup(650,650) # window size
 wn = turtle.Screen() # alias for screen
 wn.bgcolor('navy') #background colour
 
-# ! STEP 6: create player turtle
+# ! STEP 1.6: create player turtle
 player = turtle.Turtle()
 player.color('darkorange')
 player.shape('turtle')
 player.penup() # won't leave a line as the turtle moves
 
-# ! STEP 7: set speed variable
+# ! STEP 1.7: set speed variable
 speed = 1
 
-# ! STEP 8: move turtle
+# ! STEP 2.2: set keyboard binding
+turtle.listen() # listen for keystroke (onkey method)
+turtle.onkey(turn_left, 'Left')
+turtle.onkey(turn_right, 'Right')
+turtle.onkey(increase_speed, 'Up')
+
+# ! STEP 1.8: move turtle
 while True:
     player.forward(speed) # moves turtle at speed of 1
 
 # ! RUN to test
 
 # __________________________
+
 
 
